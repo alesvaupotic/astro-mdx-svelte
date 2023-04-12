511df6efac6074d44c6d51c9cb5a414476dd88a0
Aleš Vaupotič <ales@vaupotic.eu>
2023-04-12 14:12:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Astro project with Svelte in MDX
+
+https://docs.astro.build/en/guides/markdown-content/#using-components-in-mdx
---
+++ src/components/HeroImage.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let src = "__MISSING__SRC__";
+  export let alt = "__MISSING__DESCRIPTION__";
+</script>
+
+<img {src} {alt} />
+
+<style>
+  img {
+    aspect-ratio: 16 / 9;
+    width: 500px;
+    object-fit: cover;
+  }
+</style>
